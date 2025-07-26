1cafd11a5febb2b6e5908a7de2876d22bf518600
Sadhvi kesarwani <Sadhvikesarwani1@gmail.com>
2025-07-26 19:27:40 +0530

    update jwt, aggrigate, bcrypt on user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
@@ -47,5 +49,45 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+//pre middleware to generate JWT token
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("passward")) return next(); //we only want to hash the password if it has been modified or is new
+  this.passward = await bcrypt.hash(this.passward, 10); //hashing the password before saving for 10 rounds
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (passward) {
+  return await bcrypt.compare(passward, this.passward);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  jwt.sign(
+    {
+      //payload data to be included in the token
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY || "1d", //default expiry is 1 day
+    }
+  );
+};
+
+userSchema.methods.generateRefereshToken = function () {
+  jwt.sign(
+    {
+      //payload data to be included in the token
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY || "10d", //default expiry is 10 days
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
